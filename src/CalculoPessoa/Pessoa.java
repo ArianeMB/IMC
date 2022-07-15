9c99ad7558aa383b5ba4c1e19a2d0158fa721f50
Ariane Moura Barboza <arianeb@ciandt.com>
2022-07-15 20:48:57 -0300

    Comiite inicial

---
+++ src/CalculoPessoa/Pessoa.java
@@ -0,0 +1,73 @@
+package CalculoPessoa;
+
+public class Pessoa {
+	
+	//Atributos
+	
+	private String nome;
+	private double massa;
+	private double altura;
+	private	double imc;
+	
+	
+	//dentro dos parênteses são variáveis temporárias
+	//método// Construtor
+	
+	public Pessoa(String nome, double massa, double altura) {
+		this.nome = nome;
+		this.massa = massa;
+		this.altura = altura;
+		
+	}
+	
+	public void calculaImc() {
+		//Altura elevado a 2 = altura multiplicado por altura;
+		this.imc = this.massa / (this.altura * this.altura);
+		//Equivalencia de matemática altura elevado a 2 , 
+		//(this.altura * this.altura)
+		
+	}
+	
+	public String retornaCategoriaDePeso() {
+		if (this.imc < 17) {
+			return "Muito abaixo do peso";
+		}
+		if (this.imc < 18.5) {
+			return "Abaixo do peso";
+		}
+		if (this.imc < 25) {
+			return " Peso ideal";
+		}
+		if (this.imc < 30) {
+			return "Sobrepeso";
+		}
+		if (this.imc < 35) {
+			return "Obesidade";
+		}
+		if (this.imc < 40) {
+			return "Obesidade Severa";
+		}
+		
+		return "Obesidade Morbita";
+		
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public double getMassa() {
+		return this.massa;
+	}
+
+	public double getAltura() {
+		return this.altura;
+	}
+
+	public double getImc() {
+		return this.imc;
+	}
+	
+	
+
+}
